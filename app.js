f632dd3db97fed6ded58627301eb8589f0d9ec29
zgraber <zakgraber@gmail.com>
2018-03-04 01:17:10 -0800

    Working on storing images in database

--- app.js
+++ app.js
@@ -1,30 +1,59 @@
 const express = require('express');
-//const mysql = require('mysql');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+var multer = require('multer');
 const app = express();
-//const myConnection = require('express-myconnection');
 const path = require('path');
+var Schema = mongoose.Schema;
 const fs = require('fs');
-const bodyParser = require('body-parser');
-//const store = require('./store');
+//const bodyParser = require('body-parser');
 
+//const port = process.env.PORT || 8080;
 
-const port = process.env.PORT || 8080;
-/*const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  port: 3000,
-  database: 'reports'
-});
-
-app.use(myConnection(mysql, connection, 'pool')); */
-
-app.use(express.static('public'));
+/*app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(express.bodyParser({uploadDir:'/data'}));
+app.use(bodyParser({uploadDir:'/data'}));*/
+
+var url = "mongodb://27017/mydb";
+mongoose.connect(url);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+
+var pic = new picSchema({
+  img: {data: Buffer, contentType: String}
+});
+var Item = mongoose.model('Clothes', Item);
+
+app.use(multer({
+  dest: './data/',
+  rename: function (fieldname, filename) {
+    return filename;
+  }
+}));
+
+/*MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});*/
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/frontend/submitpage.html'));
+});
+
+app.post('/api/photo',function(req,res){
+ var newPic = new pic();
+ newPic.img.data = fs.readFileSync(req.files.userPhoto.path)
+ newPic.img.contentType = 'image/png';
+ newPic.save();
 });
 
 //Picture local upload
@@ -42,9 +71,9 @@ app.post('/submitsuccess', function (req, res) {
             console.error("Only .png files are allowed!");
         });
     }
-    // ...
+
 });
 
-app.listen(port, function () {
-  console.log('App listening on port: ' + port);
+app.listen(27017, function () {
+  console.log('App listening on port: 27017');
 })
