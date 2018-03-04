7c66fab8f9fa5a663cd791b7520981132ed0af9b
Alkennedy1998 <akennedy2@scu.edu>
2018-03-03 22:26:29 -0800

    Image changes

--- app.js
+++ app.js
@@ -1,49 +1,39 @@
-const express = require('express');
-//const mysql = require('mysql');
-const app = express();
-//const myConnection = require('express-myconnection');
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-//const store = require('./store');
+var express = require("express")
+var ejs = require('ejs')
+var app = express()
 
+var path = require('path')
+var multer = require('multer')
 
 const port = process.env.PORT || 8080;
-/*const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  port: 3000,
-  database: 'reports'
-});
 
-app.use(myConnection(mysql, connection, 'pool')); */
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(express.bodyParser({uploadDir:'/data'}));
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './uploadedPictures')
+	},
+	filename: function(req, file, callback) {
+		console.log(file)
+		callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+	}
+})
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/frontend/submitpage.html'));
-});
 
-//Picture local upload
-app.post('/submitsuccess', function (req, res) {
-    var tempPath = req.files.file.path,
-        targetPath = path.resolve('./uploads/image.png');
-    if (path.extname(req.files.file.name).toLowerCase() === '.png') {
-        fs.rename(tempPath, targetPath, function(err) {
-            if (err) throw err;
-            console.log("Upload completed!");
-        });
-    } else {
-        fs.unlink(tempPath, function () {
-            if (err) throw err;
-            console.error("Only .png files are allowed!");
-        });
-    }
-    // ...
-});
+app.set('view engine', 'ejs')
+
+app.get('/api/file', function(req, res) {
+	res.render('sendImage')
+})
+
+app.post('/api/file', function(req, res) {
+	var upload = multer({
+		storage: storage
+	}).single('userFile')
+	upload(req, res, function(err) {
+		res.end('File is uploaded')
+	})
+})
+
 
 app.listen(port, function () {
   console.log('App listening on port: ' + port);
