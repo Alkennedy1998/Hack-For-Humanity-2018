871cb52b5643472777591227d052e1ea7a68d409
zgraber <zakgraber@gmail.com>
2018-03-03 18:38:50 -0800

    Added image local upload functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 //const myConnection = require('express-myconnection');
 const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 //const store = require('./store');
 
@@ -20,17 +21,29 @@ app.use(myConnection(mysql, connection, 'pool')); */
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(express.bodyParser({uploadDir:'/data'}));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname + '/frontend/submitpage.html'));
 });
 
-/*app.post('/createReport', function(req, res) {
-  store.createReport({
-    //Do stuff
-  }).then(() => res.sendStatus(200));
-
-});*/
+//Picture local upload
+app.post('/submitsuccess', function (req, res) {
+    var tempPath = req.files.file.path,
+        targetPath = path.resolve('./uploads/image.png');
+    if (path.extname(req.files.file.name).toLowerCase() === '.png') {
+        fs.rename(tempPath, targetPath, function(err) {
+            if (err) throw err;
+            console.log("Upload completed!");
+        });
+    } else {
+        fs.unlink(tempPath, function () {
+            if (err) throw err;
+            console.error("Only .png files are allowed!");
+        });
+    }
+    // ...
+});
 
 app.listen(port, function () {
   console.log('App listening on port: ' + port);
