ef39c9a068db549a831ce4fb347d859cf74abbac
zgraber <zakgraber@gmail.com>
2018-03-03 20:09:29 -0800

    simple stuff

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 //const store = require('./store');
 
-
 const port = process.env.PORT || 8080;
+
 /*const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -18,19 +18,23 @@ const port = process.env.PORT || 8080;
 });
 
 app.use(myConnection(mysql, connection, 'pool')); */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(express.bodyParser({uploadDir:'/data'}));
+//app.use(bodyParser.json());
+app.use(bodyParser({uploadDir:'/data'}));
+
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/frontend/submitpage.html'));
+  res.sendFile(path.join(__dirname + '/submit-form.html'));
 });
 
 //Picture local upload
 app.post('/submitsuccess', function (req, res) {
-    var tempPath = req.files.file.path,
-        targetPath = path.resolve('./uploads/image.png');
+    var tempPath = req.file.file.path;
+    var targetPath = path.resolve('./uploads/image.png');
     if (path.extname(req.files.file.name).toLowerCase() === '.png') {
         fs.rename(tempPath, targetPath, function(err) {
             if (err) throw err;
